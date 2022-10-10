c66fc72b6d8e4966f7e5c8b8f7d5f0ac932582cf
chakeson <80765479+chakeson@users.noreply.github.com>
2022-10-11 00:15:12 +0200

    Error 404 component

--- website/src/pages/error/error.tsx
+++ website/src/pages/error/error.tsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom"
+
+
 function Error() {
-    return (  
-        <div className="">
-            Error
+    return (
+        <div className="fullscreen flex flex-col justify-center items-center">
+            <div className="flex flex-row">
+                <h1 className="font-bold text-6xl pr-6">404</h1>
+                <div className="font-medium text-xl">Looks like you got a little bit lost....<br/>Head <Link to="/" className="underline">home</Link>.</div>
+            </div>
+            <div style={{height:"70px"}}></div>{/* Increase its height. Looks better due to navbar */}
         </div>
     );
 }
