1a7a557325e20e015a29180837341c6765b05aa1
chakeson <80765479+chakeson@users.noreply.github.com>
2022-10-11 00:15:32 +0200

    Spelling fix.

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from './pages/Home/home';
+import Home from './pages/home/home';
 import Error from './pages/error/error';
 import Navbar from './compoenents/navbar';
+import Footer from './compoenents/footer';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="*" element={<Error />} />
             </Routes>
+            <Footer/>
         </BrowserRouter>
   );
 }
