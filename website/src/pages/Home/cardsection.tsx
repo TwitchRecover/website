f1664f1cdca75867f31accb70f82977d64b57252
chakeson <80765479+chakeson@users.noreply.github.com>
2022-10-16 01:19:46 +0200

    Updated card css.

--- website/src/pages/Home/cardsection.tsx
+++ website/src/pages/Home/cardsection.tsx
@@ -3,7 +3,7 @@ import cardData from "./cardData";
 
 function Cardsection() {
     return (  
-        <div className="bg-slate-300 p-10 grid gap-10 md:gap-x-0 md:gap-y-10 grid-cols-1 grid-rows-1 sm:grid-cols-2 sm:grid-rows-3 justify-items-center items-center">
+        <div className="bg-slate-300 p-10 grid gap-10 lg:gap-x-0 lg:gap-y-10 grid-cols-1 grid-rows-1 sm:grid-cols-2 sm:grid-rows-3 justify-items-center items-center">
             {cardData.map((item, index:number)=>{
                 return <Card key={index+"card"} title={item.title} subText={item.subText} linkText={item.linkText} link={item.link}/>
             })}
